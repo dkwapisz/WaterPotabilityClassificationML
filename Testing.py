21bb9d459e1b68a0e2e1a76cc8d87653fa244476
dkwapisz <dkwapisz99@gmail.com>
2024-04-13 13:13:54 +0200

    GB Classifier + balanced split

--- Testing.py
+++ Testing.py
@@ -1,29 +1,33 @@
-# Std zeros
-modelMLP_std = MLPClassifierWrapper(train_std_0, test_std_0)
-modelMLP_std.train_model()
-accuracy_MLP_std_zeros = modelMLP_std.evaluate_model()
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
-# Std mean
-modelMLP_std = MLPClassifierWrapper(train_std_mean, test_std_mean)
-modelMLP_std.train_model()
-accuracy_MLP_std_mean = modelMLP_std.evaluate_model()
+df = pd.read_csv("dataset/water_potability.csv")
 
-# Std interpolate
-modelMLP_std = MLPClassifierWrapper(train_std_interpolate, test_std_interpolate)
-modelMLP_std.train_model()
-accuracy_MLP_std_interpolate = modelMLP_std.evaluate_model()
+df.dropna(axis=0, inplace=True)
 
-# Minmax zeros
-modelMLP_minmax = MLPClassifierWrapper(train_minmax_0, test_minmax_0)
-modelMLP_minmax.train_model()
-accuracy_MLP_minmax_0 = modelMLP_minmax.evaluate_model()
+kmeans = KMeans(n_clusters = 2)
+kmeans.fit(df)
+labels = kmeans.labels_
+df_kmeans = df[labels == 1]
+df_kmeans.reset_index(inplace = True) # ???
 
-# Minmax mean
-modelMLP_minmax = MLPClassifierWrapper(train_minmax_mean, test_minmax_mean)
-modelMLP_minmax.train_model()
-accuracy_MLP_minmax_mean = modelMLP_minmax.evaluate_model()
+X = df_kmeans.iloc[:, :-1]
+y = df_kmeans.iloc[:, -1]
 
-# Minmax interpolate
-modelMLP_minmax = MLPClassifierWrapper(train_minmax_interpolate, test_minmax_interpolate)
-modelMLP_minmax.train_model()
-accuracy_MLP_minmax_interpolate = modelMLP_minmax.evaluate_model()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+sc = StandardScaler()
+X_train_sc = sc.fit_transform(X_train)
+X_test_sc = sc.transform(X_test)
+
+def accuracy(X_train, X_test, y_train, y_test):
+    gbc = GradientBoostingClassifier()
+    gbc.fit(X_train, y_train)
+    y_gbc = gbc.predict(X_test)
+    return accuracy_score(y_test, y_gbc)
+
+accuracy(X_train_sc, X_test_sc, y_train, y_test)
